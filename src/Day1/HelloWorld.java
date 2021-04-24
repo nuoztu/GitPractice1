1b2e89d2cc399c76e8d869a013f9b301789fd121
nuoztu <nuoztu@gmail.com>
2021-04-24 15:52:43 -0500

    added some more line of code again

--- src/Day1/HelloWorld.java
+++ src/Day1/HelloWorld.java
@@ -6,6 +6,7 @@ public class HelloWorld {
 
         System.out.println("Hello World");
         System.out.println("Adding GitHub now");
+        System.out.println("Stuffs are on GitHub");
 
     }
 
